3ac6f91a4b16c38f9ce3415e0362cb9435023123
hg <gudrnr2004@naver.com>
2021-05-07 11:33:42 +0900

    .

---
+++ WinFormsApp8/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace WinFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl_flename = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_server = new System.Windows.Forms.TextBox();
+            this.txt_db = new System.Windows.Forms.TextBox();
+            this.txt_pw = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_table_name = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "엑셀불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "업로드";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl_flename
+            // 
+            this.lbl_flename.AutoSize = true;
+            this.lbl_flename.Location = new System.Drawing.Point(27, 17);
+            this.lbl_flename.Name = "lbl_flename";
+            this.lbl_flename.Size = new System.Drawing.Size(39, 15);
+            this.lbl_flename.TabIndex = 2;
+            this.lbl_flename.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "서버 IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(466, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "아디";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "db이름";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(614, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "비번";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(499, 13);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 23);
+            this.txt_id.TabIndex = 7;
+            // 
+            // txt_server
+            // 
+            this.txt_server.Location = new System.Drawing.Point(203, 11);
+            this.txt_server.Name = "txt_server";
+            this.txt_server.Size = new System.Drawing.Size(100, 23);
+            this.txt_server.TabIndex = 8;
+            // 
+            // txt_db
+            // 
+            this.txt_db.Location = new System.Drawing.Point(354, 12);
+            this.txt_db.Name = "txt_db";
+            this.txt_db.Size = new System.Drawing.Size(100, 23);
+            this.txt_db.TabIndex = 9;
+            // 
+            // txt_pw
+            // 
+            this.txt_pw.Location = new System.Drawing.Point(647, 14);
+            this.txt_pw.Name = "txt_pw";
+            this.txt_pw.Size = new System.Drawing.Size(100, 23);
+            this.txt_pw.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 108);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(748, 330);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // txt_table_name
+            // 
+            this.txt_table_name.Location = new System.Drawing.Point(647, 50);
+            this.txt_table_name.Name = "txt_table_name";
+            this.txt_table_name.Size = new System.Drawing.Size(100, 23);
+            this.txt_table_name.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(562, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "입력할 테이블";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_table_name);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_pw);
+            this.Controls.Add(this.txt_db);
+            this.Controls.Add(this.txt_server);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_flename);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "INSERT";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl_flename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_db;
+        private System.Windows.Forms.TextBox txt_pw;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.TextBox txt_server;
+        private System.Windows.Forms.TextBox txt_table_name;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WinFormsApp8/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace WinFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection excel_con = null;
+            string xls_filename;
+
+            try
+            {
+                //엑셀 불러오기
+                FileDialog file_dlg = new OpenFileDialog();
+                file_dlg.InitialDirectory = "c\\";
+                file_dlg.Filter = "모든 파일 (*.*)|*.*";
+                file_dlg.RestoreDirectory = true;
+
+                //엑셀문서를 보여주기
+                if (file_dlg.ShowDialog() == DialogResult.OK)
+                {
+                    xls_filename = file_dlg.FileName;
+
+                    string str_con = "Provider = Microsoft.ACE.OLEDB.12.0.0;Data Source=" + xls_filename + ";Extended Properties='Excel 12.0;HDR=YES'";
+                    excel_con = new OleDbConnection(str_con);
+
+                    excel_con.Open();
+                    string excel_sql = @"select * from[Sheet1$]";
+
+                    OleDbDataAdapter excel_adapter = new OleDbDataAdapter(excel_sql, excel_con);
+                    DataSet excel_DS = new DataSet();
+                    excel_adapter.Fill(excel_DS);
+
+                    DataTable excel_table = excel_DS.Tables[0];
+
+                    dataGridView1.DataSource = excel_table;
+                    lbl_flename.Text = xls_filename;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("파일 가져오기 실패 : " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lbl_flename.Text == "filename")
+            {
+                MessageBox.Show("엑셀파일을 불러와 주세요.");
+            }
+            else if (txt_table_name.Text == "")
+            {
+                MessageBox.Show("저장할 테이블 명을 입력해 주세요. ");
+            }
+            else
+            {
+                OleDbConnection excel_con = null;
+
+                string server = txt_server.Text.ToString().Trim();
+                string db = txt_db.Text.ToString().Trim();
+                string id = txt_id.Text.ToString().Trim();
+                string pw = txt_pw.Text.ToString().Trim();
+                //string filename = lbl_flename.Text;
+
+                string xls_filename = lbl_flename.Text;
+                try
+                {
+                    string str_con = "Provider = Microsoft.ACE.OLEDB.12.0.0;Data Source=" + xls_filename + ";Extended Properties='Excel 12.0;HDR=YES'";
+                    excel_con = new OleDbConnection(str_con);
+
+                    excel_con.Open();
+                    string excel_sql = @"select * from[Sheet1$]";
+
+                    OleDbDataAdapter excel_adapter = new OleDbDataAdapter(excel_sql, excel_con);
+                    DataSet excel_DS = new DataSet();
+                    excel_adapter.Fill(excel_DS);
+
+                    DataTable excel_table = excel_DS.Tables[0];
+
+                    //sql에 저장하기
+                    string cnString = "Data Source=" + server + ";Initial Catalog=" + db + ";Persist Security Info=True;User ID=" + id + ";Password=" + pw;
+                    SqlConnection con = new SqlConnection(cnString);
+
+                    con.Open();
+                    SqlCommand SqlCommand = new SqlCommand();
+                    SqlCommand.Connection = con;
+
+                    //table 유무확인
+                    SqlCommand.CommandText = "SELECT COUNT(*) FROM information_schema.tables where table_name = '" + txt_table_name.Text + "' ";
+                    string tbl_exist = SqlCommand.ExecuteScalar().ToString();
+
+                    //이미 DB에 table이 있을경우
+                    if (tbl_exist == "1")
+                    {
+                        MessageBox.Show("디비에 업로드 중입니다. ");
+                        SqlCommand.CommandText = "update [uniLITE].[BPR200T] SET SAFE_STOCK_Q = 0";
+                        SqlCommand.ExecuteNonQuery();
+                    }
+
+                    for(int i = 0; i < dataGridView1.RowCount; i++)
+                    {
+                        SqlCommand.CommandText = "update [uniLITE].[BPR200T] SET SAFE_STOCK_Q = " + dataGridView1.Rows[i].Cells[1].Value.ToString() + " WHERE ITEM_CODE = '" + dataGridView1.Rows[i].Cells[0].Value.ToString() + "'";
+                        SqlCommand.ExecuteNonQuery();
+                        MessageBox.Show("업로드가 완료되었습니다..");
+                    }
+
+                    ////DB에 table이 없는 경우  data_table의 컬럼값을 읽어와 sql_table생성
+                    //else
+                    //{
+                    //    int column_count = excel_table.Columns.Count;//칼럼명
+                    //    string[] sql_column = new string[column_count];
+                    //    string sql_create = "create table [" + txt_table_name.Text.Trim() + "] ( ";
+                    //    for (int i = 0; i <= column_count - 1; i++)
+                    //    {
+                    //        string column_name = excel_table.Columns[i].ColumnName.ToString();
+                    //        sql_create = sql_create + "[" + column_name + "] varchar(50)";
+                    //        if (i != column_count - 1)
+                    //        { sql_create = sql_create + ","; }
+                    //    }
+                    //    sql_create = sql_create + ")";
+                    //    SqlCommand.CommandText = sql_create;
+                    //    SqlCommand.ExecuteNonQuery();
+
+                    //    //레코드 불러와서 insert
+                    //    if (excel_table.Rows.Count > 0)
+                    //    {
+                    //        //레코드 갯수 불러와서 그 수만큼 반복
+                    //        for (int j = 0; j <= excel_table.Rows.Count - 1; j++)
+                    //        {
+                    //            string sql_insert = " insert [" + txt_table_name.Text.Trim() + "] (";
+                    //            //data_talbe 컬럼값가져와 sql 필드명으로 대입
+                    //            for (int i = 0; i <= column_count - 1; i++)
+                    //            {
+                    //                string column_name = excel_table.Columns[i].ColumnName.ToString();
+                    //                sql_insert = sql_insert + "[" + column_name + "]";
+                    //                if (i != column_count - 1)
+                    //                { sql_insert = sql_insert + ","; }
+                    //            }
+                    //            sql_insert = sql_insert + ") values (";
+                    //            //data_table에서 입력할 필드값가져오기
+                    //            for (int i = 0; i <= column_count - 1; i++)
+                    //            {
+                    //                string value = Convert.ToString(excel_table.Rows[j][i]);
+                    //                sql_insert = sql_insert + "'" + value + "'";
+                    //                if (i != column_count - 1)
+                    //                { sql_insert = sql_insert + ","; }
+                    //            }
+                    //            sql_insert = sql_insert + ")";
+                    //            SqlCommand.CommandText = sql_insert;
+                    //            SqlCommand.ExecuteNonQuery();
+                    //        }
+                    //    }
+                    //    MessageBox.Show("업로드 완료되었습니다");
+                    //}
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("파일 저장에 실패하였습니다. :" + ex.Message);
+                }
+            }
+
+        }
+    }
+}
+
